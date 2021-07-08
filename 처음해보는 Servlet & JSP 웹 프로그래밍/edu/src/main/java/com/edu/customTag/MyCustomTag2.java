f8a8ffbc1b376307fb62d0ec2ea604ded8bf5b66
lrw <dlsodnd123@naver.com>
2021-07-08 21:16:13 +0900

    커스텀 태그 JSP 2.1 실행제어 예제

--- 처음해보는 Servlet & JSP 웹 프로그래밍/edu/src/main/java/com/edu/customTag/MyCustomTag2.java
+++ 처음해보는 Servlet & JSP 웹 프로그래밍/edu/src/main/java/com/edu/customTag/MyCustomTag2.java
@@ -10,7 +10,10 @@ public class MyCustomTag2 extends SimpleTagSupport{
 	@Override
 	public void doTag() throws JspException, IOException {
 		System.out.println("커스텀 태그의 바디가 실행되기 전");
-		getJspBody().invoke(null);
+		// JSP 1.2 기반 커스텀 태그를 작성할 때는 3개의 메소드들을 작성해야 하는데 JSP 2.0 부터는 3개의 메소드를 하나의 메소드로 처리하도록 변경
+		for(int i=0; i<10; i++) {
+			getJspBody().invoke(null);			
+		}
 		System.out.println("커스텀 태그의 바디가 실행되기 후");
 	}
 
