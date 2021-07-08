7e858ecde6c369ab2664f2fac904dccdc09ca476
lrw <dlsodnd123@naver.com>
2021-07-08 20:15:14 +0900

    커스텀 태그 JSP 1.2 실행제어 예제 - EVAL_BODY_AGAIN

--- 처음해보는 Servlet & JSP 웹 프로그래밍/edu/src/main/java/com/edu/customTag/MyCustomTag.java
+++ 처음해보는 Servlet & JSP 웹 프로그래밍/edu/src/main/java/com/edu/customTag/MyCustomTag.java
@@ -6,16 +6,23 @@ import javax.servlet.jsp.tagext.TagSupport;
 @SuppressWarnings("serial")
 public class MyCustomTag extends TagSupport {
 	
+	int cnt = 1;
+	
 	@Override
 	public int doStartTag() throws JspException {
 		System.out.println("시작태그를 만났습니다.");
-		return SKIP_BODY;	// SKIP_BODY는 태그의 몸체를 처리하지 않고 스킵
+		return EVAL_BODY_INCLUDE;
 	}
 	
 	@Override
 	public int doAfterBody() throws JspException {
-		System.out.println("body 처리가 끝났습니다.");
-		return super.doAfterBody();
+		System.out.println("body 처리가 끝났습니다. : " + cnt);
+		// EVAL_BODY_AGAIN는 몸체를 다시 실행하는 반환값
+		if(cnt++ < 10) {
+			return EVAL_BODY_AGAIN;
+		} else {
+			return SKIP_BODY;
+		}
 	}
 	
 	@Override
